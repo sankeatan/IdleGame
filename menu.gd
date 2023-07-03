f7705a06ea3a610acf8b3ebc7886ae7b09eeaaf9
unknown <cameron.clark1110@gmail.com>
2023-07-03 14:58:17 -0400

    added a menu

---
+++ menu.gd
@@ -0,0 +1,18 @@
+extends Panel
+var simultaneous_scene = preload("res://store.tscn").instantiate()
+var menu_scene = preload("res://menu.tscn").instantiate()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_new_game_button_pressed():
+	get_tree().get_root().add_child(simultaneous_scene)
+	get_tree().change_scene_to_file("res://store.tscn")
+	
